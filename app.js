5e8a8d318ab7ba473a62a8c795e97f9278a7085f
Mohamed Sharief <hello@moe-sharief.dev>
2024-03-02 21:29:10 +0300

    reworked JS 3d element

---
+++ app.js
@@ -0,0 +1,6 @@
+
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/wNrmo0wSliSAL4uQ/scene.splinecode');
