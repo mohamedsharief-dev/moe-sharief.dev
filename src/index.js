6a26efcc72c220b2ba7660bdd9c02455a2e9534e
Mohamed Sharief <hello@moe-sharief.dev>
2024-03-02 21:58:00 +0300

    updatse

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-// src/index.js
 import { Application } from '@splinetool/runtime';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('canvas3d');
-    const app = new Application(canvas);
-    app.load('https://prod.spline.design/wNrmo0wSliSAL4uQ/scene.splinecode');
-});
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/wNrmo0wSliSAL4uQ/scene.splinecode');
